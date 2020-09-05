a98a090a28c2bb5d366da4c2fac7b9108dced518
AryaAlfahrezy <33353823+Aryaalfahrezi010@users.noreply.github.com>
2020-09-05 09:25:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
    <head>
-      <title>Pwnedz By Mr.TenAr</title>
+      <title>Hacked By Mr.TenAr</title>
 <link rel="icon" type="image/x-icon" href="https://scontent.flux1-1.fna.fbcdn.net/v/t1.0-9/109185442_723119798235133_3698277027341313183_o.jpg?_nc_cat=104&_nc_sid=e3f864&_nc_eui2=AeF4_qGTyn-eCr9v5kWpltmL9WbzDadScZb1ZvMNp1Jxll555z-n8LZch-cAqAzEzbcJc0ktPfXBWCmHjcFSHhzH&_nc_ohc=L7H5gqXJ_fAAX-v6h8y&_nc_ht=scontent.flux1-1.fna&oh=5c2401b3257e5ba0d2d5d84f10fd636a&oe=5F667467">
 <meta name="title" content="$$ Party time let's have fun $$">
 <meta name="description" content="Sure You Wanna Party With Me?">
@@ -11,6 +11,6 @@
 <meta name="distribution" content="global"/>
 <meta contact='#'/>
 </head>
-Pwnedz By Mr.TenAr {{ Dark Clown Security }} <br>
+Hacked By Mr.TenAr {{ Dark Clown Security }} <br>
 <br>-= Greetz :Dark Clown Security - Pucca Cyber Team - PhantomSec1337 - BuitenzorgSyndicate - CytoXploit - Anubis Xploiter - Black Eagle Team=-
 </html>
