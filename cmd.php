56e4710cf338bbaae59ac73037fd276bf66a97a3
AryaAlfahrezy <33353823+Aryaalfahrezi010@users.noreply.github.com>
2020-09-05 09:08:04 +0700

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -115,7 +115,7 @@ if (isset($_GET["feature"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style>
             html, body {
-                background: url(https://images8.alphacoders.com/703/thumb-350-703709.png);
+                background: url(https://scontent.flux1-1.fna.fbcdn.net/v/t1.0-9/109185442_723119798235133_3698277027341313183_o.jpg?_nc_cat=104&_nc_sid=e3f864&_nc_eui2=AeF4_qGTyn-eCr9v5kWpltmL9WbzDadScZb1ZvMNp1Jxll555z-n8LZch-cAqAzEzbcJc0ktPfXBWCmHjcFSHhzH&_nc_ohc=L7H5gqXJ_fAAX-v6h8y&_nc_ht=scontent.flux1-1.fna&oh=5c2401b3257e5ba0d2d5d84f10fd636a&oe=5F667467);
                 background-size: 100%;
                 background-position: cover;
                 color: #FF4180;
